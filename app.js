596523da14489b0f15db81b5d6e3d7217fc59a15
Marco Schmalz <marco@schess.ch>
2017-09-30 12:26:50 +0200

    More explicit tokenize function with line and column number tracking.

--- app.js
+++ app.js
@@ -9,13 +9,34 @@
         if (text.split("(").length !== text.split(")").length) {
             throw SyntaxError("Parentheses missmatch.");
         }
-        return text
-            .replace(/\(/g, " ( ")
-            .replace(/\)/g, " ) ")
-            .replace(/;.*$/gm, "")
-            .replace(/\n/g, " ")
-            .split(" ")
-            .filter(Boolean);
+        let line = 1, col = 1, result = [];
+        for (let token of text.split(/(\(|\)| |\n)/)) {
+            if (token === ""){
+                //nothing
+            } else if (token === " ") {
+                col += 1;
+            } else if (token === "\n") {
+                col = 1;
+                line += 1;
+            } else if (token[0] in "1234567890".split("")) {
+                let number = new Number(token);
+                if (Number.isNaN(number.valueOf())) {
+                    throw new SyntaxError(`Not a number: ${token} (Line: ${line}, column: ${col})`);
+                }
+                number.line = line;
+                number.col = col;
+                number.length = token.length;
+                result.push(number);
+                col += token.length;
+            } else {
+                let str = new String(token);
+                str.line = line;
+                str.col = col;
+                result.push(str);
+                col += token.length;
+            }
+        }
+        return result;
     }
 
     function parse(tokens) {
@@ -23,34 +44,33 @@
         const stack = [current];
 
         for (let token of tokens) {
-            if (token === "(") {
+            if (token.valueOf() === "(") {
                 let old = current;
                 current = [];
+                current.line = token.line;
+                current.col = token.col;
+                current.colapsed = true;
+                current.toggleColapse = function() {
+                    this.colapsed = !this.colapsed;
+                };
                 old.push(current);
                 stack.push(current);
-            } else if (token === ")") {
+            } else if (token.valueOf() === ")") {
                 if (stack.length === 1) {
                     throw SyntaxError("Do not close the main 'begin' block.");
                 }
-                stack.pop();
+                let old = stack.pop();
+                old.end_line = token.line;
+                old.end_col = token.col;
                 current = stack[stack.length - 1];
             } else {
-                current.push(atom(token));
+                current.push(token);
             }
         }
         if (stack.length > 1) {
             throw SyntaxError("Program not finished."); // Should not happen
         }
         return stack[0];
-    }
-
-    function atom(token) {
-        var number = parseFloat(token);
-        if (isNaN(number)) {
-            return token;
-        } else {
-            return number;
-        }
     }
 
     const global_env = {
@@ -67,15 +87,15 @@
     };
 
     function evaluate(x, env) {
-        if (typeof x === "string") {
-            const res = env[x];
+        if (x instanceof String) {
+            const res = env[x.valueOf()];
             if (typeof res === "undefined") {
                 throw Error("Variable '" + x + "' not found");
             }
             return env[x];
-        } else if (typeof x === "number") {
-            return x;
-        } else if (x[0] === "if") {
+        } else if (x instanceof Number) {
+            return x.valueOf();
+        } else if (x[0].valueOf() === "if") {
             if (x.length != 4) {
                 throw Error("Wrong number of arguments for if: " +
                     (x.length - 1) + " != 3");
@@ -83,26 +103,26 @@
             const [_, test, conseq, alt] = x;
             const exp = evaluate(test, env) ? conseq : alt;
             return evaluate(exp, env);
-        } else if (x[0] === "define") {
+        } else if (x[0].valueOf() === "define") {
             if (x.length != 3) {
                 throw Error("Wrong number of arguments for define: " +
                     (x.length - 1) + " != 2");
             }
             const [_, name, exp] = x;
-            if (typeof name !== "string"){
+            if (!(name instanceof String)){
                 throw Error("Name of a definition is not a string: " +
                     typeof name);
             } else if ("begin define if lambda".split(" ").includes(name)) {
                 throw Error("Invalid name of a definition: " + name);
             }
-            env[name] = evaluate(exp, env);
-        } else if (x[0] === "begin") {
+            env[name.valueOf()] = evaluate(exp, env);
+        } else if (x[0].valueOf() === "begin") {
             if (x.length < 2) {
                 throw Error("At least one expression required in begin block.");
             }
             const [_, ...exps] = x;
             return exps.map(exp => evaluate(exp, env)).slice(-1)[0];
-        } else if (x[0] === "lambda") {
+        } else if (x[0].valueOf() === "lambda") {
             if (x.length != 3) {
                 throw Error("Wrong number of arguments for lambda: " +
                     (x.length - 1) + " != 2");
